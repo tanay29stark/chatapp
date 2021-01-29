21defa23539b47a1137cd090bc96e876001b9465
tanay29stark <65687595+tanay29stark@users.noreply.github.com>
2021-01-29 12:46:41 +0530

    Create Chatlist.java

---
+++ Chatlist.java
@@ -0,0 +1,18 @@
+public class Chatlist {
+   private String id;
+
+   public Chatlist(String id) {
+       this.id = id;
+   }
+
+   public Chatlist() {
+   }
+
+   public String getId() {
+       return id;
+   }
+
+   public void setId(String id) {
+       this.id = id;
+   }
+}
